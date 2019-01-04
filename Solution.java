97b48c3247fb1d628916b6421901b02feb1a5ff8
sdim-ua <ingersol@yandex.ua>
2019-01-04 13:27:49 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,14 @@
+/* 
+Печатаем строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        writeToConsole("Hello world!");
+    }
+
+    public static void writeToConsole(String s) {
+        //напишите тут ваш код
+        System.out.println("printing: " + s );
+    }
+}
